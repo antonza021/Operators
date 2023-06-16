c8ef1f6959d73c4ff1c8a6b73eb2574aadd2ee4c
User <User@WIN-AVGURKPU8OU>
2023-06-17 00:08:37 +0700

    Добавьте файлы проекта.

---
+++ Operators/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConsoleKey input;
+            RationalNumbers left = new RationalNumbers(2, 3);
+            RationalNumbers right = new RationalNumbers(3, 4);
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:\n1)Сложение\n2)Вычитание\n3)Умножение\n4)Деление\n5)Левая больше правой?\n" +
+                                  "6)Левая меньше правой?\n7)Дроби равны?\n8)Дроби не равны?");
+
+                input = Console.ReadKey().Key;
+                switch (input)
+                {
+                    case ConsoleKey.D1:
+                        RationalNumbers addition = left + right;
+                        Console.WriteLine($"Результат сложения: {addition}\n");
+                        continue;
+                    case ConsoleKey.D2:
+                        RationalNumbers substraction = left - right;
+                        Console.WriteLine($"Результат вычитания: {substraction}\n");
+                        continue;
+                    case ConsoleKey.D3:
+                        RationalNumbers multiplication = left * right;
+                        Console.WriteLine($"Результат умножения: {multiplication}\n");
+                        continue;
+                    case ConsoleKey.D4:
+                        RationalNumbers division = left / right;
+                        Console.WriteLine($"Результат деления: {division}\n");
+                        continue;
+                    case ConsoleKey.D5:
+                        bool isBigger = left > right;
+                        Console.WriteLine(isBigger + "\n");
+                        continue;
+                    case ConsoleKey.D6:
+                        bool isSmaller = left < right;
+                        Console.WriteLine(isSmaller + "\n");
+                        continue;
+                    case ConsoleKey.D7:
+                        bool isEquals = left == right;
+                        Console.WriteLine(isEquals + "\n");
+                        continue;
+                    case ConsoleKey.D8:
+                        bool isNotEquals = left != right;
+                        Console.WriteLine(isNotEquals + "\n");
+                        continue;
+                    default:
+                        break;
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ Operators/RationalNumbers.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class RationalNumbers
+    {
+        public long numerator { get; }
+        public long denominator { get; }
+
+        public RationalNumbers(long numerator, long denominator)
+        {
+            if (denominator == 0) throw new ArgumentException("The denominator cannot be equal to zero.");
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public static RationalNumbers operator +(RationalNumbers left, RationalNumbers right)
+        {
+            long numerator = left.numerator * right.denominator + right.numerator * left.denominator;
+            long denominator = left.denominator * right.denominator;
+
+            return new RationalNumbers(numerator, denominator);
+        }
+
+        public static RationalNumbers operator -(RationalNumbers left, RationalNumbers right)
+        {
+            long numerator = left.numerator * right.denominator - right.numerator * left.denominator;
+            long denominator = left.denominator * right.denominator;
+
+            return new RationalNumbers(numerator, denominator);
+        }
+
+        public static RationalNumbers operator *(RationalNumbers left, RationalNumbers right)
+        {
+            long numerator = left.numerator * right.numerator;
+            long denominator = left.denominator * right.denominator;
+
+            return new RationalNumbers(numerator, denominator);
+        }
+
+        public static RationalNumbers operator /(RationalNumbers left, RationalNumbers right)
+        {
+            if (left.denominator == 0) throw new DivideByZeroException("The denominator cannot be equal to zero.");
+
+            long numerator = left.numerator * right.denominator;
+            long denominator = right.numerator * left.denominator;
+
+            return new RationalNumbers(numerator, denominator);
+        }
+
+        public static bool operator >(RationalNumbers left, RationalNumbers right)
+        {
+            return left.numerator * right.denominator > right.numerator * left.denominator;
+        }
+
+        public static bool operator <(RationalNumbers left, RationalNumbers right)
+        {
+            return left.numerator * right.denominator < right.numerator * left.denominator;
+        }
+
+        public static bool operator ==(RationalNumbers left, RationalNumbers right)
+        {
+            return left.numerator * right.denominator == right.numerator * left.denominator;
+        }
+
+        public static bool operator !=(RationalNumbers left, RationalNumbers right)
+        {
+            return left.numerator * right.denominator != right.numerator * left.denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+    }
+}
