fc29ee7b970ecd5e29f5f510302427ad48bf6cc2
antonza021 <112742601+antonza021@users.noreply.github.com>
2023-06-17 13:05:45 +0700

    Program.cs

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -6,54 +6,54 @@ namespace Operators
     {
         static void Main()
         {
-            ConsoleKey input;
+            ConsoleKey input; // Создание переменной input
             RationalNumbers left = new RationalNumbers(2, 3);
-            RationalNumbers right = new RationalNumbers(3, 4);
+            RationalNumbers right = new RationalNumbers(3, 4);  // Создание двух объектов класса RationalNumbers
 
-            while (true)
+            while (true)  // Бесконечный цикл работы с рациональными числами
             {
                 Console.WriteLine("Выберите действие:\n1)Сложение\n2)Вычитание\n3)Умножение\n4)Деление\n5)Левая больше правой?\n" +
-                                  "6)Левая меньше правой?\n7)Дроби равны?\n8)Дроби не равны?");
+                                  "6)Левая меньше правой?\n7)Дроби равны?\n8)Дроби не равны?");   // Вывод на экран возможных операций
 
-                input = Console.ReadKey().Key;
-                switch (input)
+                input = Console.ReadKey().Key;   // Считывание ввода пользователя
+                switch (input)  // Выполнение операции в зависимости от ввода пользователя
                 {
                     case ConsoleKey.D1:
-                        RationalNumbers addition = left + right;
+                        RationalNumbers addition = left + right;   // Выполнение операции сложения
                         Console.WriteLine($"Результат сложения: {addition}\n");
                         continue;
                     case ConsoleKey.D2:
-                        RationalNumbers substraction = left - right;
+                        RationalNumbers substraction = left - right;  // Выполнение операции вычитания
                         Console.WriteLine($"Результат вычитания: {substraction}\n");
                         continue;
                     case ConsoleKey.D3:
-                        RationalNumbers multiplication = left * right;
+                        RationalNumbers multiplication = left * right;   // Выполнение операции умножения
                         Console.WriteLine($"Результат умножения: {multiplication}\n");
                         continue;
                     case ConsoleKey.D4:
-                        RationalNumbers division = left / right;
+                        RationalNumbers division = left / right;  // Выполнение операции деления
                         Console.WriteLine($"Результат деления: {division}\n");
                         continue;
                     case ConsoleKey.D5:
-                        bool isBigger = left > right;
+                        bool isBigger = left > right;  // Выполнение операции "левая больше правой"
                         Console.WriteLine(isBigger + "\n");
                         continue;
                     case ConsoleKey.D6:
-                        bool isSmaller = left < right;
+                        bool isSmaller = left < right; // Выполнение операции "левая меньше правой"
                         Console.WriteLine(isSmaller + "\n");
                         continue;
                     case ConsoleKey.D7:
-                        bool isEquals = left == right;
+                        bool isEquals = left == right; // Выполнение операции сравнения на равенство
                         Console.WriteLine(isEquals + "\n");
                         continue;
                     case ConsoleKey.D8:
-                        bool isNotEquals = left != right;
+                        bool isNotEquals = left != right; // Выполнение операции сравнения на неравенство
                         Console.WriteLine(isNotEquals + "\n");
                         continue;
                     default:
                         break;
                 }
-                break;
+                break; // Прерывание бесконечного цикла
             }
         }
     }
